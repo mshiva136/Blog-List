46224091b9520a6b6cf1d9ee971f89afadc8dc85
mshiva136 <mshiva136@gmail.com>
2022-12-23 10:20:05 +0000

    blog list

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,16 @@
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blog-list">
+      {blogsList.map(eachValue => (
+        <BlogItem key={eachValue.id} listItem={eachValue} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
